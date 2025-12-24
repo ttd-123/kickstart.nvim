70e25e3d8d58a818bfb62e59514c165ebb2aac58
ttd-123 <tomcorke14@gmail.com>
2025-12-24 16:41:55 +0000

    Changed keymap for neo-tree toggle

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   lazy = false,
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '<leader>af', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['<leader>af'] = 'close_window',
         },
       },
       filtered_items = {
